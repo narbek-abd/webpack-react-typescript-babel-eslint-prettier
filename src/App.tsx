f05470fdcbac29e47d57250ecfdfeea39d61132e
Narbek <coffee.dev.na@gmail.com>
2022-08-08 10:53:47 +0300

    cleaning up

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,5 @@
-import { lazy, Suspense } from "react";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
-
-const Cont = lazy(() => import("Src/Cont"));
-
 function App() {
-  return (
-    <>
-      <BrowserRouter>
-        <Suspense fallback={"s"}>
-          <main id="main">
-            <Routes>
-            <Route path="/catalog" element={<Cont />} />
-            </Routes>
-          </main>
-        </Suspense>
-      </BrowserRouter>
-    </>
-  );
+  return <p></p>;
 }
 
 export default App;
